58b5ac1e88f2c5d17cc35ab61629b50275026c0e
leantan-get <leantanget2@gmail.com>
2022-06-30 21:39:01 +0000

    tasks1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	printf("size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of long int: %d byte(s)\n", sizeof(long int));
 	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of float: %f byte(s)\n", sizeof(float));
+	printf("size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
 
