ccb63a3a326e11fdcd2670ffa0123234834743a9
leantan-get <leantanget2@gmail.com>
2022-06-30 21:28:02 +0000

    project3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,15 @@
 /*
  * main - Entry point
  * 
- *Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+	printf("size of a char: %d byte(s)\n",sizeof(char));
+	printf("size of an int: %d byte(s)\n",sizeof(int));
+	printf("size of long int: %d byte(s)\n",sizeof(long int));
+	printf("size of long long int: %d byte(s)\n",sizeof(long long int));
+	printf("size of float: %f byte(s)\n",sizeof(float));
+	return (0);
+}
 
