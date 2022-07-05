6e437e1dd1008cbf1e6a9c157b507b86ea635b01
leantan-get <leantanget2@gmail.com>
2022-07-05 23:23:13 +0000

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,11 +9,12 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-		c = a + b;
+		c = a * b;
 		if ((c / 10) == 0)
 		{
 		if (b == 0)
@@ -25,7 +26,7 @@ void times_table(void)
 			_putchar (' ');
 			_putchar ((c % 10) + '0');
 		}
-		if(b < 9)
+		if (b < 9)
 		{
 			_putchar (',');
 			_putchar (' ');
