5898f7609cf9655e0d87b2fc489a5a78bb5dc644
Natnael-yem <getachewnatnael036@gmail.com>
2022-07-21 18:01:53 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -31,12 +31,14 @@ int evaluate_num(int num, int iterator)
 int is_prime_number(int num)
 {
 	int iterator;
+
 	iterator = 2;
 
+	/*only greater than 2*/
 	if (num < 2)
-		{
-			return (0);
-		}
+	{
+		return (0);
+	}
 
 	if (num == 2)
 	{
