e33715e8dc05259562c2c8b61d2712431fb54131
Natnael-yem <getachewnatnael036@gmail.com>
2022-08-10 14:40:36 -0700

    advanced

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * set_string - sets the value of the pointer to a char
+ * @s: content is to copied
+ * @to: string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-this is the low level programming c projects
