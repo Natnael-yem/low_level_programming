87821d4979cfe006864b2bf32945e209f93a87e6
Natnael-yem <getachewnatnael036@gmail.com>
2022-08-27 03:29:17 -0700

    tasks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,12 +1,21 @@
 #include "lists.h"
 /**
  * free_listint - free a linked list
- * @head: listint_t list to be freed
+ * @head: pointer to the listint_t list to be freed
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
-		temp = head
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
