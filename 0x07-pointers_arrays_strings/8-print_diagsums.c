e4982367d041543300b2ff24f3a74c1879bbe016
Natnael-yem <getachewnatnael036@gmail.com>
2022-09-09 02:52:30 -0700

    task8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,12 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
+
 	i = 0;
 	while (i < size)
 	{
 		sum1 = sum1 + *(a + i * size + i);
-		sum2 = sum2 + *(a + i * size - i -1);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
 		i++;
 	}
 	printf("%i, %i\n", sum1, sum2);
