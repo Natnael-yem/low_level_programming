2895bc36f7d1c1565ae701b99459791aa8d9bb4b
Natnael-yem <getachewnatnael036@gmail.com>
2022-09-09 02:14:24 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,9 +3,9 @@
  * _strchr - loctes a character in a string
  * @s: string
  * @c: character to search
- * Return: pointer to the first occura=ence of the character c in the strings
+ * Return: pointer to the first occurrence of the character c in the strings
  **/
-char *_strchr(char *S, char c)
+char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
